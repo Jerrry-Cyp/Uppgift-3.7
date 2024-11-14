db79df81010c8a59a0dfa3481982d2e64cf42169
alsa07007 <alsa07007@E-PW05CGR5>
2024-11-14 13:03:26 +0100

    Add project files.

---
+++ Uppgift 3.7/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Globalization;
+using System.Net.Security;
+
+namespace Uppgift_3._7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Skriv en addition eller subtraction:");
+            string S = Console.ReadLine();
+            int N = S.IndexOfAny(new char[] { '+', '-' });
+            string Vänster = S.Substring(0, N);
+            string Höger = S.Substring(N + 1);
+            char tecken = S[N];
+            int Tal1 = int.Parse(Vänster);
+            int Tal2 = int.Parse(Höger);
+            
+            if (tecken == '+')
+            {
+                Console.WriteLine(Tal1 + Tal2);
+            }
+            if (tecken == '-')
+            {
+                Console.WriteLine(Tal1 - Tal2);
+            }
+            
+        }
+    }
+}
